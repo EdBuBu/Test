e3eb42ffb790a63f711f17371052e6306f9bb9c6
EdBuBu <43508519+EdBuBu@users.noreply.github.com>
2018-11-21 14:05:44 -0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello!!")
+class operation(object):
+    """docstring for sum."""
+    def sum(x,y):
+        return x + y
+    def sub(x,y):
+        return x - y
+    def mul(x,y):
+        return x * y
+    def div(x,y):
+        return x / y
+print(operation.sum(10,5))
